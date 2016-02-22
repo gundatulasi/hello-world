e59e0fe007d8229fd6780c8560f83015ee2f753a
gundatulasi <gundatulasi@gmail.com>
2016-02-22 16:33:37 -0500

    Update Exercises.java

--- Exercises.java
+++ Exercises.java
@@ -65,6 +65,17 @@ public final class Exercises {
             return (row * pascal(column - 1,row - 1)) / column;
         }
     }
+    public int pascalOther(final int column, final int row){
+    	if (column == 0 || column == row ){
+    		return 1;
+    	}else  if (column > row || column < 0 || row < 0 ){
+    		return -1;
+    	}else if ( column == 1 || row - column == 1 ){
+    		return row;
+    	}else{
+      		return pascalOther(column-1,row-1)+pascalOther(column,row-1);
+    	}
+    }
 
     public Collection<Interval> mergeIntervals(final Collection<Interval> ivals) {
         
