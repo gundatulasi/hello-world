74a0018aab5dd95bd43dc7ff96a120e3b2b7cbfa
sampathalampalli <sampath2593@gmail.com>
2016-03-07 12:23:08 -0600

    Added files via upload

---
+++ MergeSort.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mergesort;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sampath Alampalli
+ */
+public class MergeSort {
+    private int[] array;
+    private int[] tempMergArr;
+    private int length;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int k1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements you want to sort");
+        k1 = sc.nextInt();
+        int inputArr[] = new int[k1];
+        System.out.println("Enter "+k1+" number of elements");
+        for(int i=0;i<k1;i++){
+           inputArr[i] = sc.nextInt();   
+        }
+        MergeSort mms = new MergeSort();
+        mms.sort(inputArr);
+        for(int i:inputArr){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+     
+    public void sort(int inputArr[]) {
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+ 
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+         
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            doMergeSort(lowerIndex, middle);
+            doMergeSort(middle + 1, higherIndex);
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+ 
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+ 
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+ 
+    }
+
+        
+    }
+    
+
